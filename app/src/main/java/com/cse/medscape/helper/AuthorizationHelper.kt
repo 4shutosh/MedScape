efdb25f7dc7b4a3cfff441113a86a841932f1467
4shutosh <4shutoshsingh@gmail.com>
2021-06-14 22:07:44 +0530

    tweaks

--- app/src/main/java/com/cse/medscape/helper/AuthorizationHelper.kt
+++ app/src/main/java/com/cse/medscape/helper/AuthorizationHelper.kt
@@ -14,8 +14,6 @@ class AuthorizationHelper : Interceptor {
         request = request.newBuilder()
             .addHeader("Content-Type", "application/json")
             .addHeader("Accept", "application/json")
-            .addHeader("App-Id", "4ad985fc")
-            .addHeader("App-Key", "db6aa1b5f942f03379396eaeb8b6b591")
             .build()
         return chain.proceed(request)
 
